89103235f0eddc1a1a5e5b87d4fbd6a48f9d112b
Joseph Sparks <jls062015@icloud.com>
2023-12-10 20:56:45 -0500

    finished exercise 3

--- cFunctions/chap3/ex3.c
+++ cFunctions/chap3/ex3.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+
+int main()
+{
+    char word[] = 'Haywood';
+    char;
+    int i, counter = 0;
+
+    for(i = 0; word[i]; i++)
+    {
+        if(word[i]=='a'||word[i] =='e'||word[i]=='i'||word[i]=='o'||word[i]=='u'||word[i]=='A'||word[i]=='E'||word[i]=='I'||word[i]=='O'||word[i]=='U')
+        {
+            counter++;
+        }
+    }
+    printf("# of vowels in word: %d", counter);
+}
