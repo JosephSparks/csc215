376c42ce1f2cb937fde0a4de819de057d17b7839
Joseph Sparks <jls062015@icloud.com>
2023-12-15 08:54:31 -0500

    finished ex9.c

---
+++ cFunctions/dataStructures/thirdChapter/ex9.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int array[4][4] = {{1,2,3,4},
+                        {2,3,4,5},
+                        {3,4,5,6},
+                        {4,5,6,7}};
+
+    int i,j;
+    int arraySize = sizeof(array) / sizeof(array[0]);
+
+    int product = 1;
+
+    for(i=0,j=1;i<arraySize && j<arraySize;i++,j++){
+        product *= array[i][j];
+    }
+
+    printf("Product of values above the main diagonal: %d\n", product);
+}
