8f2a121bda9377f759e02b24aeca8b0933d2181b
Joseph Sparks <jls062015@icloud.com>
2024-01-09 16:52:22 -0500

    low2up complete

--- cFunctions/dataStructures/fourthChapter/acc_string.c
+++ cFunctions/dataStructures/fourthChapter/acc_string.c
@@ -11,3 +11,16 @@ int acc_strlen(char *str){
     }
     return i;
 }
+
+// Exercise 2
+int acc_low2up(char *str){
+    char str[100], upStr[100];
+    int i = 0;
+    while(str[i] != '\0'){
+        if(str[i] >= 'a' && str[i] <= 'z')
+            upStr[i] = str[i] - 32;
+        else
+            upStr[i] = str[i];
+        i++;
+    }
+}
