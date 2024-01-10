2380fb2129af1151595ae6952373150cd434bc26
Joseph Sparks <jls062015@icloud.com>
2024-01-10 11:33:18 -0500

    revise

--- cFunctions/dataStructures/fourthChapter/acc_string.c
+++ cFunctions/dataStructures/fourthChapter/acc_string.c
@@ -8,7 +8,7 @@ int main(){
     if(strlen(str) == acc_strlen(str)){
         printf("Success!");
     } else{
-        printf("Failure");
+        printf("Better luck next time!");
     }
 
     printf("\nTesting acc_strlow2up\n");
@@ -21,8 +21,10 @@ int main(){
     printf("%s", acc_strcat(str,str2));
 
     printf("\nTesting acc_strcmp\n");
-    if(acc_strcmp(str,str2) != strcmp(str,str2)){
-        printf("Failure");
+    printf("%d", acc_strcmp(str,"Astrophyzicz"));
+    printf("%d", strcmp(str,"Astrophyzicz"));
+    if(acc_strcmp(str,"Astrophyzicz") != strcmp(str,"Astrophyzicz")){
+        printf("Better luck next time!");
     }
     else {
         printf("Success!");
