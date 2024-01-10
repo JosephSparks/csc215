5c26b9408d241be2dfc167dad8c4d106eb4eae42
Joseph Sparks <jls062015@icloud.com>
2024-01-09 22:49:35 -0500

    Updated Ex4 and created test cases

--- cFunctions/dataStructures/fourthChapter/acc_string.c
+++ cFunctions/dataStructures/fourthChapter/acc_string.c
@@ -11,5 +11,15 @@ int main(){
     else{
         printf("Failure");
     }
+
+    printf("\nTesting acc_strlow2up\n");
+    printf("%s",acc_low2up(str));
+
+    printf("\nTesting acc_strcat\n");
+    char str2[30] = "Chinese";
+    printf("%s", acc_strcat(str,str2));
+
+    printf("\nTesting acc_strcmp\n");
+    return 0;
 }
 
--- cFunctions/dataStructures/fourthChapter/acc_string.h
+++ cFunctions/dataStructures/fourthChapter/acc_string.h
@@ -42,23 +42,27 @@ char* acc_strcat(char *destinationStr, char *sourceStr){
 }
 
 // Exercise 4
-int acc_strcmp(char *str1, char *str2){
-    int i = 0;
-    int len1=strlen(str1), len2=strlen(str2);
-    while(i < len1 && i < len2){
-        if(str1[i] != str2[i]){
-            if(str1[i] > str2[i]){
-                // String 1 is GREATER than String 2
-                return 1;
-            }
-            else{
-                // String 2 is GREATER than String 1
-                return -1;
+int acc_strcmp(const char *str1, const char *str2) {
+    int i = 0, len1 = strlen(str1), len2 = strlen(str2);
+
+    while (i < len1 && i < len2) {
+        if (str1[i] != str2[i]) {
+            if (str1[i] > str2[i]) {
+                return 1; // str1 is greater than str2
+            } else {
+                return -1; // str2 is greater than str1
             }
         }
+        i++;
     }
-    i++;
-    return 0;
+
+    if (len1 == len2) {
+        return 0; // Both strings are equal
+    } else if (len1 > len2) {
+        return 1; // str1 is greater than str2
+    } else {
+        return -1; // str2 is greater than str1
+    }
 }
 
 // Exercise 5
