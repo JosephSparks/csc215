09cc69186a5ddf4534b3592cf390a3ca7adc77cc
Joseph Sparks <jls062015@icloud.com>
2024-01-16 21:47:41 -0500

    finalized ex21.c

--- cFunctions/dataStructures/fifthChapter/ex21.c
+++ cFunctions/dataStructures/fifthChapter/ex21.c
@@ -3,22 +3,18 @@
 
 int main()
 {
-    char str[100]; 
-    int i,j;
+    char str[100];
     printf ("\n Enter the string: "); 
     fgets(str, 100, stdin); 
 
-    for(int i = 0; str[i] != '\0'; i++) {
-        if(str[i] == ';') {
-            for(int j = i; j++;) {
-                str[j] = str[j + 1];
-            }
-            i--; // Move back to recheck the current position
+    for(int i = 0; str[i] != '\0'; i++){
+        if(str[i] == ';'){
+            str[i] = str[i + 1];
+            } // Move back to recheck the current position
         }
-    }
 
     // Period to commas
-    for(int i = 0; str[i] != '\0'; i++) {
+    for(int i = 0; str[i] != '\0'; i++){
         if (str[i] == '.') {
             str[i] = ',';
         }
