9729d4fcdade4db192f80313f198a452876b91ad
Joseph Sparks <jls062015@icloud.com>
2024-01-22 20:22:27 -0500

    Update nestedStudents.c

--- C/dataStructures/chap5/nestedStudents.c
+++ C/dataStructures/chap5/nestedStudents.c
@@ -1,48 +1,43 @@
 #include <stdio.h>
 
+
+struct DOB{
+    int day;
+    int month;
+    int year;
+};
+
+struct student{
+    int roll_no;
+    char name[100];
+    float fees;
+    struct DOB date;
+};
+    
+struct student createStudent()
+{
+    struct student stud1;
+
+    printf("\nEnter the roll number: ");
+    scanf("%d", &stud1.roll_no);
+    printf("Enter the name: ");
+    scanf("%s", stud1.name);  // Using %[^\n] to read a line with spaces
+    printf("Enter the fees: ");
+    scanf("%f", &stud1.fees);
+    printf("Enter the student's birth month, day, and year separated by spaces: ");
+    scanf("%d %d %d", &stud1.date.month, &stud1.date.day, &stud1.date.year);
+
+    return stud1;
+}
+
 int main()
 {
-    struct DOB{
-        int day;
-        int month;
-        int year;
-    };
+    struct student stud1 = createStudent();
 
-    struct student{
-        int roll_no;
-        char name[100];
-        float fees;
-        struct DOB date;
-    };
-    
-    struct student createStudent()
-    {
-        struct student stud1;
-
-        printf("\nEnter the roll number: ");
-        scanf("%d", &stud1.roll_no);
-        printf("Enter the name: ");
-        scanf(" %s", stud1.name);  // Using %[^\n] to read a line with spaces
-        printf("Enter the fees: ");
-        scanf("%f", &stud1.fees);
-        printf("Enter the DOB: ");
-        scanf(" %d %d %d", &stud1.date.day, &stud1.date.month, &stud1.date.year);
-
-        return stud1;
-    }
-
-    printf("\n Enter the roll number: ");
-    scanf("%d", &stud1.roll_no);
-    printf("\n Enter the name: ");
-    scanf("%s", stud1.name);
-    printf("\n Enter the fees: ");
-    scanf("%f", &stud1.fees);
-    printf("\n Enter the DOB: ");
-    scanf("%d %d %d", &stud1.date.day, &stud1.date.month, &stud1.date.year);
     printf("\n ********STUDENT'S DETAILS *******");
     printf("\n ROLL No. = %d", stud1.roll_no);
     printf("\n NAME = %s", stud1.name);
-    printf("\n FEES = %f", stud1.fees);
-    printf("\n DOB = %d-%d-%d", stud1.date.day, stud1.date.month, stud1.date.year); getch();
+    printf("\n FEES = %.2f", stud1.fees);
+    printf("\n DOB = %d-%d-%d\n", stud1.date.month, stud1.date.day, stud1.date.year);
     return 0;
 }
